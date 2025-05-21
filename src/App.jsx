ad449473e5c231ea6a5a4b86d9cb5c221aa8b5d6
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-05-21 21:45:25 +0300

    react web1 start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Virsraksts from './Virsraksts';
+
+function App() {
+  return (
+    <div className="App" id="app">
+      <Virsraksts />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Virsraksts.jsx
@@ -0,0 +1,12 @@
+import './virsraksts.css';
+export default function Virsraksts(){
+    return(
+        <>
+            <h1>Hello world</h1>
+            <h2>This is my new page</h2>
+            <h3>This is my new page</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam nostrum unde necessitatibus nam numquam beatae, sapiente perferendis, dolorum eaque velit ratione cum deleniti voluptatibus provident, ipsum tempora quaerat odit hic.</p>
+        </>
+    )
+}
+
