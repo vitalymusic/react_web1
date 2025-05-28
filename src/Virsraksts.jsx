a550296084b61cecf54fc97502193c510a82aae9
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-05-28 18:06:51 +0300

    react last

--- src/Virsraksts.jsx
+++ src/Virsraksts.jsx
@@ -2,7 +2,7 @@ import './virsraksts.css';
 export default function Virsraksts(){
     return(
         <>
-            <h1>Hello world</h1>
+            <h1 className='border-solid border-b-amber-500 border-2 text-amber-400'>Hello world</h1>
             <h2>This is my new page</h2>
             <h3>This is my new page</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam nostrum unde necessitatibus nam numquam beatae, sapiente perferendis, dolorum eaque velit ratione cum deleniti voluptatibus provident, ipsum tempora quaerat odit hic.</p>
