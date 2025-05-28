f8dac18446267bd0b4807df0c845e6e35c89d9ad
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-05-28 21:13:39 +0300

    posts

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -6,29 +6,48 @@ function Posts() {
 
     // 1. Funkcijas kas veido vaicājumu uz: https://jsonplaceholder.typicode.com/posts/
     // 2. Funkcijas kas ielādē datus, kad React components tiek ielādēts
-    
-    
-    
-    async function loadPosts(){
+
+
+
+    async function loadPosts() {
         let response = await fetch('https://jsonplaceholder.typicode.com/posts/');
         let data = await response.json();
         return data;
+
     }
 
     const [posts, setPosts] = useState([]);
+
     useEffect(() => {
-        setPosts(loadPosts())
-    },[])
+        loadPosts().then((data) => {
+            setPosts(data);
+        });
 
+    }, [])
 
-
-    return (
-    <>
-        <h1>Ziņas</h1>  
-
-
-    </>
-    );
+    if (!posts) {
+        return (
+            <>
+                <h1>Ziņas</h1>
+                <p>ielādē...</p>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h1>Ziņas</h1>
+                {posts.map((post, i) => {
+                    return (
+                        <div className='post'>
+                            <h3>{post.title}</h3>
+                            <p>{post.body}</p>
+                            <button data-id={post.id}>Skatīt postu</button>
+                        </div>)
+                })
+                }
+            </>
+        )
+    }
 }
 
 export default Posts;
